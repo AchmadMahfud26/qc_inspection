3ff9b003762aa1400817494e38665bc12f96487e
AchmadMahfud26 <achmadmahfud01@gmail.com>
2026-08-17 23:49:55 +0700

    Rf

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -14,25 +14,25 @@ $user = current_user();
 
     <ul class="nav nav-pills flex-column">
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/index.php"><i class="fa fa-tachometer-alt"></i> <span class="nav-label ms-2">Dashboard</span></a></li>
-        <li class="nav-item mt-3"><strong class="text-muted ps-2">Master Data</strong></li>
+        <li class="nav-item mt-3"><strong class="text-white ps-2">Master Data</strong></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/admin/products/index.php"><i class="fa fa-boxes"></i> <span class="nav-label ms-2">Produk</span></a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/admin/customers/index.php"><i class="fa fa-building"></i> <span class="nav-label ms-2">Customer</span></a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/admin/defects/index.php"><i class="fa fa-ban"></i> <span class="nav-label ms-2">Defect</span></a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/admin/inspection_items/index.php"><i class="fa fa-list"></i> <span class="nav-label ms-2">Inspection Item</span></a></li>
 
-        <li class="nav-item mt-3"><strong class="text-muted ps-2">Inspection</strong></li>
+        <li class="nav-item mt-3"><strong class="text-white ps-2">Inspection</strong></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/inspection/after_welding/index.php"><i class="fa fa-fire-extinguisher"></i> <span class="nav-label ms-2">After Welding</span></a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/inspection/after_painting/index.php"><i class="fa fa-paint-roller"></i> <span class="nav-label ms-2">After Painting</span></a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/inspection/final_check/index.php"><i class="fa fa-check-square"></i> <span class="nav-label ms-2">Final Check</span></a></li>
 
-        <li class="nav-item mt-3"><strong class="text-muted ps-2">Data & Analysis</strong></li>
+        <li class="nav-item mt-3"><strong class="text-white ps-2">Data & Analysis</strong></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/data_inspection.php"><i class="fa fa-table"></i> <span class="nav-label ms-2">Data Inspection</span></a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/analysis/defect_analysis.php"><i class="fa fa-bug"></i> <span class="nav-label ms-2">Analisa Defect</span></a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/analysis/product_analysis.php"><i class="fa fa-boxes"></i> <span class="nav-label ms-2">Analisa Produk</span></a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/analysis/process_analysis.php"><i class="fa fa-cog"></i> <span class="nav-label ms-2">Analisa Proses</span></a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/analysis/pareto.php"><i class="fa fa-chart-line"></i> <span class="nav-label ms-2">Pareto Defect</span></a></li>
 
-        <li class="nav-item mt-3"><strong class="text-muted ps-2">Lainnya</strong></li>
+        <li class="nav-item mt-3"><strong class="text-white ps-2">Lainnya</strong></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/reports/index.php"><i class="fa fa-file-alt"></i> <span class="nav-label ms-2">Report</span></a></li>
         <li class="nav-item"><a class="nav-link text-white" href="/qc_inspection/auth/logout.php"><i class="fa fa-sign-out-alt"></i> <span class="nav-label ms-2">Logout</span></a></li>
     </ul>
